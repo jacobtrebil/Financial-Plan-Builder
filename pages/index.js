6d086638b5fba669edde5b744d87dbfeea62c53d
Jacob <jacobtrebil@yahoo.com>
2021-04-03 13:11:14 -0500

    cleared dashboard to simplify

--- pages/index.js
+++ pages/index.js
@@ -26,31 +26,9 @@ export function App() {
     <div>
      <div className="app">
         <div id="header">
-          <h1 className="h1">Jacob's Dashboard</h1>
+          <h1 className="h1">Jacob's Plans</h1>
         </div>
         <div id="left-section">
-            <div id="tracking-section">
-              <div id="march2021">
-              <p className="p" id="march">March 2021 </p>
-              </div>
-              <div id="timelines">
-              <p className="times p">1D</p>
-              <p className="times p">1W</p>
-              <p className="times p">1M</p>
-              <p className="times p">3M</p>
-              <p className="times p">1Y</p>
-              <p className="times p">ALL</p>
-            </div>
-              <div id="bar1-section">
-                <div id="bar1"></div>
-            </div>
-              <p className="p" id="earnings">$4,350 Earned</p>
-              <p className="p" id="savings">$3,116 / $2,500 Saved</p>
-              <div id="bar2-section">
-                <div id="bar2"></div>
-              </div>
-              <p id="spendings" className="p">$1,234 Spent</p>
-              </div>
           <div id="plans-section">
             <p className="p">Plans</p>
             <div id="plan1">
@@ -58,29 +36,6 @@ export function App() {
               <p className="p" id="monthly-savings">Invest $450 a Month in 401(k)</p>
             </div>
             <button id="dashboard-add-plan-button">+ Add Plan</button>
-          </div>
-        </div>
-        <div id="right-section">
-          <p className="p">Assets</p>
-          <img className="chart" src="/images/graph.png" width={300} height={250} />
-          <GraphComponent />
-          <div className="asset-box">
-            <div className="asset-section">
-              <p className="p asset">401(k)</p>
-              <p className="p asset-number">$183,540</p>
-            </div>
-            <div className="percent-box">
-              <p className="percent-change">+4.5% (1M)</p>
-            </div>
-          </div>
-          <div className="asset-box">
-            <div className="asset-section2">
-              <p className="p asset2">Investments</p>
-              <p className="p asset-number2">$10,952</p>
-            </div>
-            <div className="percent-box">
-              <p className="percent-change red">-2.5% (1M)</p>
-            </div>
           </div>
         </div>
         <NavComponent />
