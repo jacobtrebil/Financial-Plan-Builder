372081df08bd8f570a4a24b920c5b9c54b8a6a52
Jacob <jacobtrebil@yahoo.com>
2021-04-07 13:39:54 -0500

    worked on form validation for kids page

--- pages/section-1-kids.js
+++ pages/section-1-kids.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import _dynamic from 'next/dynamic';
+import { useForm} from 'react-hook-form';
+import { Alert } from 'react-bootstrap';
 
 const NavComponent = _dynamic(() =>
   import('../components/nav').then((mod) => mod.SideBar)
@@ -15,16 +17,22 @@ function Kids() {
 
     const [showForm, setShowForm] = useState(false)
     const [showForm2, setShowForm2] = useState(false)
-
-    function addToDatabase() {
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = (data) => {
+      alert(JSON.stringify(data));
     }
 
     return (
       <div>
         <h1 id="plan-form-h1">Do you have kids?</h1>
         <h2 id="plan-form-h2"></h2>
-        <form id="plan-form-page-1" onSubmit={addToDatabase}>
-            <select className="custom-select" defaultValue="No" onChange= {() => setShowForm(!showForm) }>
+        <form id="plan-form-page-1" onSubmit={handleSubmit(onSubmit)}>
+            <select 
+            {...register('kids', {required: true})}
+            name="kids" 
+            className="custom-select" 
+            defaultValue="No" 
+            onChange= {() => setShowForm(!showForm) }>
                 <option>Yes</option>
                 <option>No</option>
             </select><br></br>
@@ -33,7 +41,7 @@ function Kids() {
         <div>
       <div class="plan-input-box">
       <label className="retirement-form-label">How many kids do you have?</label>
-      <select defaultValue="2">
+      <select name="numberofkids" {...register('kids', {required: true})} defaultValue="2">
           <option>1</option>
           <option>2</option>
           <option>3</option>
@@ -43,7 +51,12 @@ function Kids() {
       </div>
       <div className="plan-input-box">
         <label className="retirement-form-label">Do you plan to help pay for your kids college?</label>
-        <select className="custom-select" defaultValue="No" onChange= {() => setShowForm2(!showForm2) }>
+        <select 
+        name="payforkids" 
+        {...register('kids', {required: true})} 
+        className="custom-select" 
+        defaultValue="No" 
+        onChange= {() => setShowForm2(!showForm2) }>
                 <option>Yes</option>
                 <option>No</option>
             </select><br></br>
@@ -52,14 +65,21 @@ function Kids() {
       showForm2 && (
         <div>
           <div className="plan-input-box">
-            <label className="retirement-form-label">How much will you spend on each of your kids college?</label>
-            <input></input>
+            <label name="kidscollegespending" className="retirement-form-label">How much will you spend on each of your kids college?</label>
+            <input 
+            name="kidsspending" 
+            {...register('kids', {required: true})} 
+            placeholder="100000" 
+            defaultValue="10000" 
+            type="number">
+            </input>
+            {errors?.kidsspending && <p>This field is required</p>}
           </div>
         </div>
       )}
       </div>
       )}
-            <Link href="/section-1-supporting-others"><button id="plan-button">Next &#8594;</button></Link>
+      <button type="submit" id="plan-button">Next &#8594;</button>
         </form>
         <NavComponent />
         <FooterComponent />
