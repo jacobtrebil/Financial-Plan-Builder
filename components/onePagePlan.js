bf8854cb7720d86ad80706fcfa158929f65c98c2
Jacob <jacobtrebil@yahoo.com>
2021-07-14 19:52:16 -0500

    improved the final financial plan page

--- components/onePagePlan.js
+++ components/onePagePlan.js
@@ -78,8 +78,43 @@ export function onePagePlan(plan) {
               <h1 className="financialPlanHeadline">{calculations.firstName}'s Financial Plan</h1>
               <p className="financialPlanSubheadline">Your plan shows with your future income, future spending, how your net worth will change over time, <br></br>as well as key info for you to know regarding your financial future.</p>
             <div>
+              <h1 className="planHeadline">Key Info</h1>
+              <p className="planSubheadline">See important information regarding your financial future</p>
+              <p className="keyInfoP">Retire at age: {calculations.retirementAge}</p>
+              <p className="keyInfoP">Take Social Security at age: {calculations.socialSecurityAge}</p>
+              <p className="keyInfoP">Risk Tolerance: {calculations.riskScore}</p>
+              <p className="keyInfoP">Based on your risk tolerance, we recommend you invest in the portfolio below</p>
+            </div>
+            <div>
               <h1 className="planHeadline">Future Income</h1>
               <p className="planSubheadline">See how much you're projected to earn in the future</p>
+              <p className="chartHeadline">Future Income</p>
+              <p className="chartSubheadline">Including Purchase Goals & Healthcare Expenses</p>
+              <AreaChart
+                className="barChart"
+                width={550}
+                height={180}
+                data={data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              >
+              <XAxis name="Age" dataKey="Age" stroke="grey" fontSize="12px" />
+              <YAxis
+                name="Expenses"
+                stroke="grey"
+                fontSize="12px"
+                dataKey="Expenses"
+                tickFormatter={toUSDThousands}
+              />
+              <Tooltip cursor={{ stroke: 'black' }} fontSize="12px" content={CustomTooltipToThousands}/>
+              <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+              <Area
+                dataKey="Expenses"
+                fontSize="12px"
+                fill="rgb(4, 187, 172)"
+                stroke="rgb(4, 187, 172)"
+              />
+              </AreaChart>
+              <p className="chartDescription">Age</p>
             </div>
             <div>
               <h1 className="planHeadline">Future Spending</h1>
@@ -104,10 +139,10 @@ export function onePagePlan(plan) {
               <Tooltip cursor={{ stroke: 'black' }} fontSize="12px" content={CustomTooltipToThousands}/>
               <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
               <Area
-              dataKey="Expenses"
-              fontSize="12px"
-              fill="rgb(4, 187, 172)"
-              stroke="rgb(4, 187, 172)"
+                dataKey="Expenses"
+                fontSize="12px"
+                fill="rgb(4, 187, 172)"
+                stroke="rgb(4, 187, 172)"
               />
               </AreaChart>
               <p className="chartDescription">Age</p>
@@ -129,10 +164,10 @@ export function onePagePlan(plan) {
                   See what you will spend during retirement
                 </p>
                 <hr className="purchaseGoalsHr"></hr>
-                <p>Living Expense: {calculations.livingExpense}</p>
-                <p>Your living expenses will increase 2% a year due to inflation</p>
-                <p>Healthcare Expenses: {calculations.healthcareExpense}</p>
-                <p>Your healthcare expenses will increase 5% a year as you get older</p>
+                <p className="retirementSpendingExpense">Living Expense: <b>{toUSDThousands(calculations.livingExpense)}</b></p>
+                <p className="retirementSpendingIncrease">Your living expenses will increase 2% a year due to inflation</p>
+                <p className="retirementSpendingExpense">Healthcare Expenses: <b>{toUSDThousands(calculations.healthcareStartingExpense)}</b></p>
+                <p className="retirementSpendingIncrease">Your healthcare expenses will increase 5% a year as you get older</p>
               </div>
             </div>
             <div>
@@ -167,33 +202,27 @@ export function onePagePlan(plan) {
                 <p className="chartDescription">Age</p>
             </div>
             <div>
-              <h1 className="planHeadline">Key Info</h1>
-              <p className="planSubheadline">See important information regarding your financial future</p>
-              <p>Retire at age: {calculations.retirementAge}</p>
-              <p>Take Social Security at age: {calculations.socialSecurityAge}</p>
-            </div>
-            <div>
               <h1 className="planHeadline">Add Important Documents</h1>
               <p className="planSubheadline">Upload Documents to store safely in your Financial Plan</p>
               <div className="planDocumentUploadBox">
                 <p className="planDocumentUploadType">Tax Plan</p>
                 <p className="noPlanDocumentFound">*No Documents Found</p>
-                <button className="planDocumentUploadButton">+ Upload Plan</button>
+                <button type="file" className="planDocumentUploadButton">+ Upload Plan</button>
               </div>
               <div className="planDocumentUploadBox">
                 <p className="planDocumentUploadType">Estate Plan</p>
                 <p className="noPlanDocumentFound">*No Documents Found</p>
-                <button className="planDocumentUploadButton">+ Upload Plan</button>
+                <button type="file" className="planDocumentUploadButton">+ Upload Plan</button>
               </div>
               <div className="planDocumentUploadBox">
                 <p className="planDocumentUploadType">Your Will</p>
                 <p className="noPlanDocumentFound">*No Documents Found</p>
-                <button className="planDocumentUploadButton">+ Upload Will</button>
+                <button type="file" className="planDocumentUploadButton">+ Upload Will</button>
               </div>
               <div className="planDocumentUploadBox">
                 <p className="planDocumentUploadType">Insurance</p>
                 <p className="noPlanDocumentFound">*No Documents Found</p>
-                <button className="planDocumentUploadButton">+ Upload Insurance</button>
+                <button type="file" className="planDocumentUploadButton">+ Upload Insurance</button>
               </div>
         </div>
       </div>
